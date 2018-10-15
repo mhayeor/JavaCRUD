86f0b6c3923c2419b4105dd09c0752b13a3c7e24
TiFe Pariola <mhayeor@gmail.com>
2018-10-15 11:31:58 +0100

    Updated CreateStudentTable to try with resources

--- src/main/java/model/StudentDao.java
+++ src/main/java/model/StudentDao.java
@@ -16,33 +16,14 @@ public class StudentDao implements InterfaceDao<Student> {
     }
 
     public void createStudentTable () {
-        Connection connection = null;
-        Statement statement = null;
 
-        try {
-            connection = ConnectionConfiguration.getConnection();
-            statement = connection.createStatement();
-            statement.execute("CREATE TABLE IF NOT EXISTS student (id int primary key unique auto_increment," +
-                    "first_name varchar(55), last_name varchar(55))");
+        try (Connection connection = ConnectionConfiguration.getConnection();
+        Statement statement = connection.createStatement()) {
+        statement.execute("CREATE TABLE IF NOT EXISTS student (id int primary key unique auto_increment," +
+                "first_name varchar(55), last_name varchar(55))");
 
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (statement != null) {
-                try {
-                    statement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
     public void save(Student student) {
